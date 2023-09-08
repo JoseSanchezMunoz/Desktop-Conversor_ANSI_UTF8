694b9f134665573d62dba58f8e5b7e491e577e0c
JoseSanchezMunoz <josesanchezmunoz056@gmail.com>
2023-09-08 11:42:57 -0500

    v2
    Barra de progreso y descripcion actualizada

--- interfaz_grafica.py
+++ interfaz_grafica.py
@@ -1,9 +1,14 @@
-#interfaz_grafica.py
+# interfaz_grafica
+# Se define la interfaz de usuario con etiquetas, cuadros de entrada y botones.
+# Permite seleccionar rutas de origen y destino, así como codificaciones.
+# Incluye una barra de progreso para mostrar el estado de la conversión.
+
 # Importamos las bibliotecas y módulos necesarios.
-import tkinter as tk  # Importamos la biblioteca tkinter para crear la interfaz gráfica.
-from tkinter import filedialog, ttk  # Importamos filedialog para abrir cuadros de diálogo de selección de directorio y ttk para widgets mejorados.
-from funcionalidades import Funcionalidades  # Importamos la clase Funcionalidades desde el archivo funcionalidades.py
-import os  # Importamos el módulo os para trabajar con rutas y directorios.
+import tkinter as tk
+from tkinter import filedialog, ttk, messagebox
+from funcionalidades import Funcionalidades
+import os
+import codecs
 
 # Definimos la clase InterfazGrafica, que gestionará la interfaz de usuario.
 class InterfazGrafica:
@@ -44,6 +49,15 @@ class InterfazGrafica:
         self.button_carpeta_destino = tk.Button(self.ventana, text="Abrir Carpeta Destino", command=self.abrir_carpeta_destino)
         self.button_convertir = tk.Button(self.ventana, text="Convertir", command=self.convertir_archivos)
 
+        # Creamos una etiqueta para mostrar el progreso.
+        self.label_progreso = tk.Label(self.ventana, text="Progreso:")
+        self.label_progreso.grid(row=5, column=0, padx=10, pady=5)
+
+        # Creamos una barra de progreso.
+        self.barra_progreso = ttk.Progressbar(self.ventana, length=200, mode="determinate")
+        self.barra_progreso.grid(row=5, column=1, padx=10, pady=5)
+        self.barra_progreso["value"] = 0  # Inicializamos la barra de progreso en 0.
+
         # Colocamos los elementos en la ventana utilizando la cuadrícula.
         self.label_carpeta_origen.grid(row=0, column=0, padx=10, pady=5)
         self.label_carpeta_destino.grid(row=1, column=0, padx=10, pady=5)
@@ -81,28 +95,45 @@ class InterfazGrafica:
         carpeta_origen = self.entry_carpeta_origen.get()
         carpeta_destino = self.entry_carpeta_destino.get()
 
-        # Si las entradas están vacías, utilizamos las rutas por defecto.
         if not carpeta_origen:
             carpeta_origen = self.carpeta_origen_por_defecto
         if not carpeta_destino:
             carpeta_destino = self.carpeta_destino_por_defecto
 
-        # Verificamos si las rutas de origen y destino existen.
         if not os.path.exists(carpeta_origen) or not os.path.exists(carpeta_destino):
             self.mostrar_mensaje_error("Ingrese rutas de origen y destino válidas.")
             return
 
-        # Realizamos la conversión según la codificación seleccionada.
-        if codificacion_origen == codificacion_destino:
-            self.mostrar_mensaje_informacion("Mismo formato detectado. No se realizará la conversión.")
-            return
+        archivos_a_convertir = [f for f in os.listdir(carpeta_origen) if f.endswith('.txt')]
+        errores = []
+
+        # Configuramos la barra de progreso.
+        self.barra_progreso["maximum"] = len(archivos_a_convertir)
+        self.barra_progreso["value"] = 0  # Inicializamos la barra de progreso en 0.
 
         funcionalidades = Funcionalidades()
 
-        if codificacion_origen == "ANSI" and codificacion_destino == "UTF-8":
-            funcionalidades.convertir_a_utf8(carpeta_origen, carpeta_destino)
-        elif codificacion_origen == "UTF-8" and codificacion_destino == "ANSI":
-            funcionalidades.convertir_a_ansi(carpeta_origen, carpeta_destino)
+        for idx, archivo in enumerate(archivos_a_convertir, start=1):
+            try:
+                with codecs.open(os.path.join(carpeta_origen, archivo), 'r', 'ansi') as archivo_origen:
+                    contenido = archivo_origen.read()
+
+                with codecs.open(os.path.join(carpeta_destino, archivo), 'w', 'utf-8') as archivo_destino:
+                    archivo_destino.write(contenido)
+            except Exception as e:
+                errores.append(f"Error al convertir {archivo}: {str(e)}")
+
+            # Actualizamos la barra de progreso.
+            self.barra_progreso["value"] = idx
+            self.ventana.update()  # Actualizamos la ventana para mostrar el progreso.
+
+        if errores:
+            self.mostrar_mensaje_error("Se produjeron errores al convertir algunos archivos:\n\n" + "\n".join(errores))
+        else:
+            self.mostrar_mensaje_informacion("La conversión se completó exitosamente.")
+        
+        # Restauramos la barra de progreso después de la conversión.
+        self.barra_progreso["value"] = 0
 
     # Función para mostrar un mensaje de error.
     def mostrar_mensaje_error(self, mensaje):
